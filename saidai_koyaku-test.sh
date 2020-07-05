3fab9fcea479fff5ee69fae3cc6598f33d336b93
higashi <higashun1122@gmail.com>
2020-07-05 23:15:15 +0900

    shell script update

--- saidai_koyaku-test.sh
+++ saidai_koyaku-test.sh
@@ -4,22 +4,17 @@
 # 最大公約数が6と正しく出力されるかどうかを調べるためのシェルスクリプトである
 
 
-# 変数化
+# 変数
 a=$1
 b=$2
-
 ans="/tmp/$$-ans"
 result="/tmp/$$-result"
 err="/tmp/$$-error" 
 
-# テスとの正常動作の確認
+# テストの正常動作の確認
 echo "6" > ${ans}
 ./saidai_koyaku.sh $a $b > ${result}
-#./saidai_koyaku.sh $a $b > ${result}
 diff ${ans} ${result} || echo "error in 1-1" >> ${err}
-
-#if [ ${ans} -eq ${result} ]; then
-#       	echo "error in 1-1" >> ${err}
 
 # エラーログがある場合
 if [ -f ${err} ]; then
